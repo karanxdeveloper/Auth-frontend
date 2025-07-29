5c9760cd5c7dae43fbf265b97b9671b8b29609fd
karan <karan372003@gmail.com>
2025-07-30 02:20:45 +0530

    made home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import EmailVerify from "./pages/EmailVerify"
+import ResetPassword from "./pages/ResetPassword"
 
 
 function App() {
   return (
     <>
-     <div className="text-red-600 text-xl">
-      This is auth frontend
+     <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/email-verify' element={<EmailVerify/>} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
      </div>
     </>
   )
